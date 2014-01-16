6520b2c5d76f80cf15d5143c5388d8ae16297975
Eivind Hyldmo <Eivind.93@hotmail.com>
2014-01-16 14:56:02 +0100

    Reworte more of the camera scripting and player controls

--- Assets/Scripts/CameraFollow2.cs
+++ Assets/Scripts/CameraFollow2.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class CameraFollow2 : MonoBehaviour 
 {
-
-	public float xSmooth = 10f;		// How smoothly the camera catches up with it's target movement in the x axis.
-	public float ySmooth = 10f;		// How smoothly the camera catches up with it's target movement in the y axis.
-	public float speedFactor = 10f;
+	public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
+	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
+	public float xSmooth = 4f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	public float ySmooth = 4f;		// How smoothly the camera catches up with it's target movement in the y axis.
 	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
 	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
-	public bool facingRight;
+	public bool facingRight = false;
+	public float cameraOffset = -6;
 
 	private Transform player;		// Reference to the player's transform.
 
@@ -32,18 +33,25 @@ public class CameraFollow2 : MonoBehaviour
 		float targetX = transform.position.x;
 		float targetY = transform.position.y;
 		//Debug.Log("X: "+player.position.x+", Y: "+player.position.x);
-
+		//if (CheckXMargin()) 
+		//Debug.Log("Vel: "+Mathf.RoundToInt(player.rigidbody2D.velocity.x) + ", FR = "+facingRight + ", CO = "+Mathf.Abs(player.rigidbody2D.velocity.x/2) * Time.deltaTime);
+			if((player.rigidbody2D.velocity.x > 0 && !facingRight) || (player.rigidbody2D.velocity.x < 0 && facingRight))  {
+			facingRight = !facingRight;
+			cameraOffset *= -1; 
+			}
 			// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
 		targetX = Mathf.Lerp(
 			transform.position.x, 
-			player.position.x+Mathf.Clamp(player.rigidbody2D.velocity.x*(speedFactor*0.1f), -7f, 7f), 
-			xSmooth * Time.deltaTime
+			cameraOffset+player.position.x
+			//+Mathf.Clamp(player.rigidbody2D.velocity.x*(speedFactor*0.1f), -6f, 6f)
+			,(Mathf.Abs(player.rigidbody2D.velocity.x/4)+1.5f) * Time.deltaTime
 			);
-		Debug.Log("Vel: "+player.rigidbody2D.velocity.x);
+
+
 			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
 		targetY = Mathf.Lerp(
 			transform.position.y, 
-			2f + player.position.y+Mathf.Clamp(player.rigidbody2D.velocity.y*(speedFactor*0.05f),-1000, 0), 
+			2f + player.position.y+Mathf.Clamp(player.rigidbody2D.velocity.y*(ySmooth*0.05f),-10, 0), 
 			ySmooth * Time.deltaTime
 			);
 
@@ -55,4 +63,16 @@ public class CameraFollow2 : MonoBehaviour
 		transform.position = new Vector3(targetX, targetY, transform.position.z);
 	}
 
+	bool CheckXMargin()
+	{
+		// Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
+		return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
+	}
+	
+	
+	bool CheckYMargin()
+	{
+		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
+		return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+	}
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,147 +3,169 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-	[HideInInspector]
-	public bool facingRight = true;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
-	
+    [HideInInspector]
+    public bool
+        facingRight = true;         // For determining which way the player is currently facing.
+    [HideInInspector]
+    public bool
+        jump = false;               // Condition for whether the player should jump.
+    
 
-	public float moveForce = 3*150f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 6f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 3*200f;			// Amount of force added when the player jumps.
+    [HideInInspector]
+    public float
+        moveForce = 150f;       // Amount of force added to move the player left and right.
+    [HideInInspector]
+    public float
+        runSpeed = 10f;     // The fastest the player can travel in the x axis.
+    public float maxSpeed;
+    public float jumpForce = 3 * 200f;          // Amount of force added when the player jumps.
 
-	private float gravScale;
+    private float gravScale;
+    private Transform groundCheck;          // A position marking where to check if the player is grounded.
+    private Transform wallCheck;
+    private bool grounded = false;  // Whether or not the player is grounded.
+    private bool walled = false;
+    private bool falling = false;
+    private Animator anim;
 
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private Transform wallCheck;
+    void Awake ()
+    {
+        // Setting up references.
+        gravScale = rigidbody2D.gravityScale;
+        groundCheck = transform.Find ("groundCheck");
+        wallCheck = transform.Find ("wallCheck");
+        anim = GetComponent<Animator> ();
+    }
 
-	private bool grounded = false;	// Whether or not the player is grounded.
-	private bool walled = false;
-	
-	private bool falling = false;	
-	private Animator anim;
+    void Update ()
+    {
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));  
+        walled = Physics2D.Linecast (transform.position, wallCheck.position, 1 << LayerMask.NameToLayer ("Walls")); 
 
-	void Awake()
-	{
-		// Setting up references.
-		gravScale = rigidbody2D.gravityScale;
-		groundCheck = transform.Find("groundCheck");
-		wallCheck = transform.Find("wallCheck");
-		anim = GetComponent<Animator>();
-	}
+        anim.SetBool ("Walled", walled);
 
+        anim.SetBool ("Grounded", grounded);
+        anim.SetBool ("Falling", falling);
 
-	void Update()
-	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
-		walled = Physics2D.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Walls")); 
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        if (Input.GetButtonDown ("Jump") && (grounded || walled)) {
+            jump = true;
+            falling = false;
+        }
+        
+        if (Input.GetButton ("Sprint") && grounded) {
+            maxSpeed = runSpeed * 1.5f;
+        } else if (grounded) {
+            maxSpeed = runSpeed;
+        }
+    }
 
-		anim.SetBool("Walled", walled);
+    void FixedUpdate ()
+    {
+        //If the player is stuck to the wall and falling
+        if (walled && falling) {
+            //Reduce the gravity by 200%
+            rigidbody2D.gravityScale = gravScale / 4;
+        } else {
+            //Return it to normal
+            rigidbody2D.gravityScale = gravScale;
+        }
+        // Cache the horizontal input.
+        float h = Input.GetAxis ("Horizontal");
 
-		anim.SetBool("Grounded", grounded);
-		anim.SetBool("Falling", falling);
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        anim.SetFloat ("Speed", Mathf.Abs (h * 2));
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && (grounded || walled)){
-			jump = true;
-			falling = false;
-		}
-		
-		if (Input.GetButton("Sprint") && grounded) {
-			maxSpeed = 10f;
-		} else if (grounded) {
-			maxSpeed = 6f;
-		}
-	}
+        //If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+        if (h * rigidbody2D.velocity.x < maxSpeed && (Mathf.Abs (rigidbody2D.velocity.x) < maxSpeed)) {
+            // ... add a force to the player.
+            if (grounded) {
+                rigidbody2D.AddForce (Vector2.right * h * moveForce);
+            } else { 
+                rigidbody2D.AddForce ((Vector2.right * h * moveForce) / 5);
+            }
+        }
 
+        // If the player's horizontal velocity is greater than the maxSpeed...
+        if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed) {
+            // ... set the player's velocity to the maxSpeed in the x axis.
+            rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+        }
 
-	void FixedUpdate ()
-	{
-		//If the player is stuck to the wall and falling
-		if (walled && falling) {
-			//Reduce the gravity by 200%
-			rigidbody2D.gravityScale = gravScale/4;
-		} else {
-			//Return it to normal
-			rigidbody2D.gravityScale = gravScale;
-		}
-		// Cache the horizontal input.
-		float h = Input.GetAxis("Horizontal");
+        //TODO 
+        else if (h == 0 && grounded) {
+            rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x * 0.90f, rigidbody2D.velocity.y);
+        } else if (h == 0) {
+            rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x * 0.985f, rigidbody2D.velocity.y);
+        }
+        
+        if (grounded) {
+            // If the input is moving the player right and the player is facing left...
+            if (h > 0 && !facingRight) {
+                // ... flip the player.
+                Flip ();
+                // Otherwise if the input is moving the player left and the player is facing right...
+            } else if (h < 0 && facingRight) {
+                // ... flip the player.
+                Flip ();
+            }
+        } else {
+            if (h > 0 && rigidbody2D.velocity.x > 1 && !facingRight) {
+                Flip ();
+                Debug.Log("flip flap "+Mathf.Round(rigidbody2D.velocity.x));
+                // Otherwise if the input is moving the player left and the player is facing right...
+            } else if (h < 0 && rigidbody2D.velocity.x < -1 && facingRight) {
+                Debug.Log("flip flap "+Mathf.Round(rigidbody2D.velocity.x));
+                   // ... flip the player.
+                Flip ();
+            }
+        }
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h*2));
+        // If the player should jump...
+        if (jump) {
+            //While on the ground:
+            if (grounded) {
+                // Set the Jump animator trigger parameter.
+                anim.SetTrigger ("Jump");
 
-		//If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed )
-			// ... add a force to the player.
-			if (grounded)
-			rigidbody2D.AddForce(Vector2.right * h * moveForce);
-			else 
-			rigidbody2D.AddForce((Vector2.right * h * moveForce) / 15);
+                // Add a vertical force to the player.
+                rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
 
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+                // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+                jump = false;
+            //While in the air and attached to a wall
+            } else if (walled) {
+                //Disable sprinting in air
+                maxSpeed = runSpeed;
+                //Reset the speed before the jump
+                rigidbody2D.velocity = new Vector2 (0, 0);
+                anim.SetTrigger ("Jump");
+                if (facingRight) {
+                    rigidbody2D.AddForce (new Vector2 (-jumpForce * 2.5f, jumpForce / 1.2f));
+                } else {
+                    rigidbody2D.AddForce (new Vector2 (jumpForce * 2.5f, jumpForce / 1.2f));
+                }
+                Flip ();
+                jump = false;
+            }
 
-		//TODO 
-		else if (h == 0 && grounded) {
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x * 0.90f, rigidbody2D.velocity.y);
-		} else if (h == 0) {
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x * 0.985f, rigidbody2D.velocity.y);
-		}
-		// If the input is moving the player right and the player is facing left...
-		if(rigidbody2D.velocity.x > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(rigidbody2D.velocity.x < 0 && facingRight)
-			// ... flip the player.
-			Flip();
+        } else if (rigidbody2D.velocity.y < -1) {
+            falling = true;
+        } else if (grounded) {
+            falling = false;
+        }
 
-		// If the player should jump...
-		if(jump)
-		{
-			if (grounded) {
-			// Set the Jump animator trigger parameter.
-			anim.SetTrigger("Jump");
+    }
+    
+    void Flip ()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
 
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
-			} else if (walled) {
-				anim.SetTrigger("Jump");
-				rigidbody2D.velocity = new Vector2(0, 0);
-				if (facingRight) {
-					rigidbody2D.AddForce(new Vector2(-jumpForce*2.5f, jumpForce/1.2f));
-				}else {
-					rigidbody2D.AddForce(new Vector2(jumpForce*2.5f, jumpForce/1.2f));
-				}
-				Flip();
-				jump = false;
-			}
-
-		} else if (rigidbody2D.velocity.y < -1) {
-			falling = true;
-		} else if (grounded){
-			falling = false;
-		}
-
-	}
-	
-	
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
-
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
